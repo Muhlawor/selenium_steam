032c1c4bbce22cbaf5d24a339fab326fb2755dc2
Pin <vasya93@gmail.com>
2023-02-07 10:16:14 +0200

    Replaced XPATH selection to CSS_SELECTOR

--- main_optimized.py
+++ main_optimized.py
@@ -7,8 +7,8 @@ from selenium.webdriver import ChromeOptions
 import undetected_chromedriver as uc
 
 opts = ChromeOptions()
-opts.add_argument("--window-size=1400,1000")
-opts.add_argument("--user-data-dir=C:\\Users\\ASUS_TUF_DASH_F15\\AppData\\Local\\Google_2\\Chrome\\User Data")
+opts.add_argument("--window-size=1800,1400")
+opts.add_argument("--user-data-dir=C:\\Users\\vasya\\AppData\\Local\\Google_2\\Chrome\\User Data")
 opts.add_argument('--profile-directory=Profile 69')
 
 driver = uc.Chrome(options=opts)
@@ -20,7 +20,7 @@ driver.get("https://steamdb.info/sales/?cc=ar&min_reviews=0&min_rating=0&min_dis
 # Function to filter the sales by 2 year lows
 def filter_2_year_lows():
     filter_button = WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located(
-        (By.XPATH, "/html/body/div[5]/div[2]/div[2]/div[2]/div[1]/form/div/div[4]/div[3]/div")))
+        (By.CSS_SELECTOR, "#js-discounts-minor > div")))
     filter_button.click()
 
 
@@ -28,14 +28,14 @@ def filter_2_year_lows():
 # Function to sort the sales by price
 def sort_by_price():
     sort_button = WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located(
-        (By.XPATH, "/html/body/div[5]/div[2]/div[2]/div[2]/div[2]/div[2]/div/div[2]/table/thead/tr/th[5]")))
+        (By.CSS_SELECTOR, "#DataTables_Table_0 > thead > tr > th:nth-child(5)")))
     sort_button.click()
 
 
 # Function to add a specific sale to the cart
 def add_to_cart(sale_index):
-    sale_link = WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.XPATH, 
-        f"/html/body/div[5]/div[2]/div[2]/div[2]/div[2]/div[2]/div/div[2]/table/tbody/tr[{sale_index}]/td[1]/a")))
+    sale_link = WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,
+        f"#DataTables_Table_0 > tbody > tr:nth-child({sale_index}) > td:nth-child(1) > a")))
     sale_link.click()
     driver.switch_to.window(driver.window_handles[1])
     try:
@@ -53,9 +53,9 @@ def add_to_cart(sale_index):
     driver.switch_to.window(driver.window_handles[0])
 
 
-num_sales_to_add = 100
+num_sales_to_add = 10
 
-
+#time.sleep(999)
 
 # Apply the filters and sort
 filter_2_year_lows()
