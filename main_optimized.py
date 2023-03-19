0b8845c3fb25b66a66232ad08d7cc34929ed753d
Pin <vasya93@gmail.com>
2023-03-19 15:02:59 +0200

    refactor...

--- main_optimized.py
+++ main_optimized.py
@@ -15,37 +15,76 @@ STEAM_STORE_CART_URL = "https://store.steampowered.com/cart/"
 TWO_YEAR_LOWS_SELECTOR = "#js-discounts-minor > div"
 PRICE_COLUMN_SELECTOR = "#DataTables_Table_0 > thead > tr > th:nth-child(5)"
 
-opts = ChromeOptions()
-opts.add_argument("--window-size=1800,1400")
-opts.add_argument("--user-data-dir=C:\\Users\\vasya\\AppData\\Local\\Google_2\\Chrome\\User Data")
-opts.add_argument('--profile-directory=Profile 69')
 
-driver = uc.Chrome(options=opts)
-driver.get(STEAMDB_URL)
+def main():
+    opts = ChromeOptions()
+    opts.add_argument("--window-size=1800,1400")
+    opts.add_argument("--user-data-dir=C:\\Users\\vasya\\AppData\\Local\\Google_2\\Chrome\\User Data")
+    opts.add_argument('--profile-directory=Profile 69')
+
+    driver = uc.Chrome(options=opts)
+    driver.get(STEAMDB_URL)
+
+    filter_2_year_lows(driver)
+    sort_by_price(driver)
+
+    num_sales_to_add = 30
+    max_price = 50
+    max_total_sum = 122
+
+    num_sales_added = 0
+    total_sum = 0
+    for i in range(1, num_sales_to_add + 1):
+        previous_sum = total_sum
+        total_sum = add_to_cart(driver, i, max_price, total_sum, max_total_sum)
+        # If the total_sum has increased, increment the num_sales_added counter
+        if total_sum > previous_sum:
+            num_sales_added += 1
+
+    # Print the final values for num_sales_added and total_sum after the loop
+    print(f"\nNumber of sales added: {num_sales_added}")
+    print(f"Total price of sales added: ARS$ {total_sum:.2f}\n")
+
+    driver.switch_to.new_window('tab')
+    driver.get(STEAM_STORE_CART_URL)
+    input("Press enter to close the browser...")
+
+    # Close the browser
+    driver.quit()
+
 
 def filter_2_year_lows(driver):
+    """Click the '2 year lows' filter button on the SteamDB sales page."""
     filter_button = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, TWO_YEAR_LOWS_SELECTOR)))
     filter_button.click()
 
+
 def sort_by_price(driver):
+    """Sort the SteamDB sales table by price."""
     sort_button = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, PRICE_COLUMN_SELECTOR))
     )
     sort_button.click()
 
+
 def get_sale_price(driver, sale_index):
+    """Get the sale price of the item at the given index in the SteamDB sales table."""
     SALE_PRICE_SELECTOR = f"#DataTables_Table_0 > tbody > tr:nth-child({sale_index}) > td:nth-child(5)"
     sale_price_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, SALE_PRICE_SELECTOR)))
     sale_price = float(sale_price_element.text.replace("ARS$ ", "").replace(',', '.'))
     return sale_price
 
+
 def click_sale_link(driver, sale_index):
-    SALE_LINK_SELECTOR = f"#DataTables_Table_0 > tbody > tr:nth-child({sale_index}) > td:nth-child(1) > a"
+    """Click the link of the sale item at the given index in the SteamDB sales table."""
+    SALE_LINK_SELECTOR = f"#DataTables_Table_0 > tbody > tr:nth-child({sale_index}) > td:nth-child(1) >a"
     sale_link = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, SALE_LINK_SELECTOR)))
     sale_link.click()
 
-def add_to_cart(driver, sale_index, max_price, total_sum):
+
+def add_to_cart(driver, sale_index, max_price, total_sum, max_total_sum):
+    """Add a sale item to the cart if it meets the price criteria."""
     sale_price = get_sale_price(driver, sale_index)
 
     if sale_price < max_price:
@@ -74,7 +113,9 @@ def add_to_cart(driver, sale_index, max_price, total_sum):
 
     return total_sum
 
-def handle_add_to_cart_process(driver,sale_index, total_sum):
+
+def handle_add_to_cart_process(driver, sale_index, total_sum):
+    """Handle the process of adding a sale item to the cart."""
     ADD_TO_CART_SELECTOR = "a[id^='btn_add_to_cart_'] > span"
 
     try:
@@ -92,30 +133,7 @@ def handle_add_to_cart_process(driver,sale_index, total_sum):
 
     return total_sum
 
-num_sales_to_add = 30
-max_price = 50
-max_total_sum = 122
 
-filter_2_year_lows(driver)
-sort_by_price(driver)
-
-num_sales_added = 0
-total_sum = 0
-for i in range(1, num_sales_to_add + 1):
-    previous_sum = total_sum
-    total_sum = add_to_cart(driver, i, max_price, total_sum)
-    # If the total_sum has increased, increment the num_sales_added counter
-    if total_sum > previous_sum:
-        num_sales_added += 1
-
-# Print the final values for num_sales_added and total_sum after the loop
-print(f"\nNumber of sales added: {num_sales_added}")
-print(f"Total price of sales added: ARS$ {total_sum:.2f}\n")
-
-driver.switch_to.new_window('tab')
-driver.get(STEAM_STORE_CART_URL)
-input("Press enter to close the browser...")
-
-# Close the browser
-driver.quit()
+if __name__ == "__main__":
+    main()
 
