73ed2d704937c5c59890e135ca115a92103ea615
Pin <vasya93@gmail.com>
2023-03-19 05:59:13 +0200

    added max sum feature, refactoring

--- main_optimized.py
+++ main_optimized.py
@@ -1,10 +1,14 @@
+# Standard library imports
 import time
+
+# Third-party library imports
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver import ChromeOptions
 import undetected_chromedriver as uc
+import threading
 
 opts = ChromeOptions()
 opts.add_argument("--window-size=1800,1400")
@@ -16,34 +20,80 @@ driver = uc.Chrome(options=opts)
 driver.get("https://steamdb.info/sales/?cc=ar&min_reviews=0&min_rating=0&min_discount=0&category=29")
 
 
-def filter_2_year_lows():
+def filter_2_year_lows(driver):
+    """
+    This function clicks the '2 year lows' filter button on the SteamDB sales page.
+    :param driver: The Selenium webdriver instance.
+    """
     filter_button = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "#js-discounts-minor > div")))
     filter_button.click()
 
 
-def sort_by_price():
+def sort_by_price(driver):
+    """
+    Sort the SteamDB sales table by price.
+
+    :param driver: The Selenium webdriver instance.
+    """
+    # CSS selector for the price column header in the SteamDB sales table
+    PRICE_COLUMN_SELECTOR = "#DataTables_Table_0 > thead > tr > th:nth-child(5)"
+
+    # Wait for the price column header to be present and click on it to sort the table
     sort_button = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "#DataTables_Table_0 > thead > tr > th:nth-child(5)")))
+        EC.presence_of_element_located((By.CSS_SELECTOR, PRICE_COLUMN_SELECTOR))
+    )
     sort_button.click()
 
 
-def add_to_cart(sale_index, max_price):
-    sale_price_selector = f"#DataTables_Table_0 > tbody > tr:nth-child({sale_index}) > td:nth-child(5)"
-    sale_price_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, sale_price_selector)))
+def add_to_cart(driver, sale_index, max_price, total_sum):
+    """
+    Add a sale item to the cart based on its index in the SteamDB sales table.
+
+    :param driver: The Selenium webdriver instance.
+    :param sale_index: The index of the sale item in the SteamDB sales table.
+    :param max_price: The maximum price allowed for a sale item.
+    :param total_sum: The current total sum of sale items in the cart.
+    :return: The updated total sum after adding the sale item to the cart.
+    """
+    SALE_PRICE_SELECTOR = f"#DataTables_Table_0 > tbody > tr:nth-child({sale_index}) > td:nth-child(5)"
+    sale_price_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, SALE_PRICE_SELECTOR)))
     sale_price = float(sale_price_element.text.replace("ARS$ ", "").replace(',', '.'))
+
+    # Log the sale price and update the total sum when the item is added to the cart
+    if sale_price < max_price:
+        if total_sum + sale_price > max_total_sum:
+            print(f"Sale {sale_index}: Sale price is ARS$ {sale_price}, Total sum would exceed the max total sum, skipping...")
+            return total_sum
+        else:
+            total_sum += sale_price
+            print(f"Sale {sale_index}: Sale price is ARS$ {sale_price}, Total sum: ARS$ {round(total_sum, 2)}")
+    else:
+        print(f"Sale {sale_index}: Sale price is ARS$ {sale_price}, Total sum: ARS$ {round(total_sum, 2)}")
+        return total_sum
 
     if sale_price >= max_price:
         print(f"Sale {sale_index} is above the price threshold, skipping...")
         return
 
-    sale_link_selector = f"#DataTables_Table_0 > tbody > tr:nth-child({sale_index}) > td:nth-child(1) > a"
-    sale_link = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, sale_link_selector)))
+    SALE_LINK_SELECTOR = f"#DataTables_Table_0 > tbody > tr:nth-child({sale_index}) > td:nth-child(1) > a"
+    sale_link = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, SALE_LINK_SELECTOR)))
     sale_link.click()
     driver.switch_to.window(driver.window_handles[1])
 
+    if total_sum < max_total_sum:
+        total_sum = handle_add_to_cart_process(driver, sale_index, total_sum)
+    else:
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
+
+    return total_sum
+
+def handle_add_to_cart_process(driver, sale_index, total_sum):
+    ADD_TO_CART_SELECTOR = "a[id^='btn_add_to_cart_'] > span"
+
     try:
-        add_to_cart_button = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a[id^='btn_add_to_cart_'] > span")))
+        add_to_cart_button = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, ADD_TO_CART_SELECTOR)))
         if "In Cart" in add_to_cart_button.get_attribute("innerHTML"):
             print(f"Sale {sale_index} is already in the cart")
         else:
@@ -55,19 +105,35 @@ def add_to_cart(sale_index, max_price):
         driver.close()
         driver.switch_to.window(driver.window_handles[0])
 
+    return total_sum
 
-num_sales_to_add = 50
-max_price = 50
 
-filter_2_year_lows()
-sort_by_price()
+num_sales_to_add     = 30
+max_price            = 50
+max_total_sum        = 100
 
+
+
+filter_2_year_lows(driver)
+sort_by_price(driver)
+
+num_sales_added = 0
+total_sum = 0
 for i in range(1, num_sales_to_add + 1):
-    add_to_cart(i, max_price)
+    previous_sum = total_sum
+    total_sum = add_to_cart(driver, i, max_price, total_sum)
+    # If the total_sum has increased, increment the num_sales_added counter
+    if total_sum > previous_sum:
+        num_sales_added += 1
+
+
+# Print the final values for num_sales_added and total_sum after the loop
+print(f"\nNumber of sales added: {num_sales_added}")
+print(f"Total price of sales added: ARS$ {round(total_sum, 2)}\n")
 
 driver.switch_to.new_window('tab')
 driver.get("https://store.steampowered.com/cart/")
-time.sleep(999)
+input("Press enter to close the browser...")
 
 # Close the browser
-# driver.quit()
+driver.quit()
